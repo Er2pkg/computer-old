864a9cfb43409945ac10ff493b9739fd630b7b9c
Er2ch <er2off@outlook.com>
2020-04-04 13:57:10 +0300

    (:)

--- cmds/rank.js
+++ cmds/rank.js
@@ -1,7 +1,7 @@
 ﻿module.exports.info = {
 name: 'ранк',
 engname: 'rank',
-regex: '/р[ау]н[кг]/',
+regex: '/[рт][ау]н[кг]/',
 engregex: '/r[au]n[kg]/',
 args: '[человек]',
 engargs: '[member]',
@@ -16,6 +16,6 @@ if(!user) user = message.author
 Comp.con.query(`SELECT * FROM xp WHERE id = ${user.id}`, (err, rows) => {
 message.channel.stopTyping()
 if(rows.length < 1) return message.reply(ph[0])
-message.channel.send(`XP: ${rows[0].xp}\n${ph[1]+rows[0].lvl}`)
+message.channel.send(`XP: ${rows[0].xp}/${Comp.xpFormule(rows[0].lvl)}\n${ph[1]+rows[0].lvl}`)
 })
 }
