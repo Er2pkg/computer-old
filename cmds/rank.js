b81634752bf5d80cf3ed2fc78cb5c7fc3f802e17
Er2ch <er2off@outlook.com>
2020-04-06 22:48:24 +0300

    .........

--- cmds/rank.js
+++ cmds/rank.js
@@ -32,7 +32,7 @@ await bg
 .composite(bar, 255, 210)
 .print(fnt, 255, 146, user.tag)
 .print(fnt, 655, 0, 'lvl: ' + row.lvl)
-.print(fnt, 350, 50, row.xp + '/' + (5 * (rlvl ^ 2) + 50 * rlvl + 100) + ' xp')
+.print(fnt, 350, 50, row.xp + '/' + Comp.xpFormule(row.lvl) + ' xp')
 .print(fnt, 245, 0, 'money:$' + (row.money.toString().length > 3?row.money.toString().slice(0, -(rpr.money.toString().length - 3)) + 'K':row.money))
 .getBuffer(Comp.jimp.MIME_PNG, async(err, buff) => {
 await message.channel.stopTyping();
