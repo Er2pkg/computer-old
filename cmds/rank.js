7ba25ac51005559b1db30fe17313ccde5b7057c5
Er2ch <er2off@outlook.com>
2020-04-06 22:44:57 +0300

    FFF

--- cmds/rank.js
+++ cmds/rank.js
@@ -16,17 +16,17 @@ const rcard = async (row, length, timer) => {
 await message.channel.startTyping()
 const {Canvas} = require('canvas-constructor')
 Comp.jimp.read(user.avatarURL).then(async avatar => {
-await Comp.jimp.read('../assets/avatarmask.png').then(async mask => {
+await Comp.jimp.read('./assets/avatarmask.png').then(async mask => {
 await avatar.resize(200, 200).mask(mask, 0, 0)
-await Comp.jimp.read('../assets/bgmask.png').then(async bgmask => {
+await Comp.jimp.read('./assets/bgmask.png').then(async bgmask => {
 await Comp.jimp.read(row.bg).then(async bg => {
 await bg.resize(934, 282).blur(5).mask(bgmask)
 await Comp.jimp.read(new Canvas(700, 20).setColor('#' + bg.getPixelColor(96, 100).toString(16).slice(0, -2)).addRect(0, 0, Math.ceil(length), 20).toBuffer()).then(async bar => {
-await Comp.jimp.read('../assets/xpmask.png').then(async xpmask => await bar.mask(xpmask.resize(700, 20), 0, 0))
-await bar.resize(634, 40); if(user.id == Comp.owners.stalin) await Comp.jimp.read('../assets/staff.png').then(async sicon => await bg.composite(sicon, 55, 5))
+await Comp.jimp.read('./assets/xpmask.png').then(async xpmask => await bar.mask(xpmask.resize(700, 20), 0, 0))
+await bar.resize(634, 40); if(user.id == Comp.owners.stalin) await Comp.jimp.read('./assets/staff.png').then(async sicon => await bg.composite(sicon, 55, 5))
 //govnokod
-if(user.presence.status === 'online') await Comp.jimp.read('../assets/online.png').then(async online => await avatar.composite(online, 141, 151)); if(user.presence.status === 'idle') await Comp.jimp.read('../assets/idle.png').then(async idle => await avatar.composite(idle, 141, 151)); if(user.presence.status === 'offline') await Comp.jimp.read('../assets/invisible.png').then(async offline => await avatar.composite(offline, 141, 151)); if(user.presence.status === 'dnd') await Comp.jimp.read('../assets/dnd.png').then(async dnd => await avatar.composite(dnd, 141, 151))
-await Comp.jimp.loadFont('../fonts/uni-sans-heavy-64-white.fnt').then(async fnt => {
+if(user.presence.status === 'online') await Comp.jimp.read('./assets/online.png').then(async online => await avatar.composite(online, 141, 151)); if(user.presence.status === 'idle') await Comp.jimp.read('./assets/idle.png').then(async idle => await avatar.composite(idle, 141, 151)); if(user.presence.status === 'offline') await Comp.jimp.read('./assets/invisible.png').then(async offline => await avatar.composite(offline, 141, 151)); if(user.presence.status === 'dnd') await Comp.jimp.read('./assets/dnd.png').then(async dnd => await avatar.composite(dnd, 141, 151))
+await Comp.jimp.loadFont('./fonts/uni-sans-heavy-64-white.fnt').then(async fnt => {
 await bg
 .composite(avatar, 50, 50)
 .composite(bar, 255, 210)
