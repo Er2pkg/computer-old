07c5306420cc4f9b08b21cc956eb79c615e7390f
Er2ch <er2off@outlook.com>
2020-04-24 10:45:01 +0300

    some rank changes

--- cmds/rank.js
+++ cmds/rank.js
@@ -12,21 +12,21 @@ module.exports.run = async (message, ph) => {
 let user
 if(message.args[0]) user = Comp.client.users.cache.get(message.args[0]) || message.mentions.users.first()
 if(!user) user = message.author
-const rcard = async (row, length, timer) => {
+const rcard = async row => {
 message.channel.startTyping()
-const {Canvas} = require('canvas-constructor')
-Comp.jimp.read(user.avatarURL({format: 'png'})).then(async avatar => {
-await Comp.jimp.read('./assets/avatarmask.png').then(async mask => {
+const {Canvas} = require('canvas-constructor'),
+avatar = await Comp.jimp.read(user.avatarURL({format: 'png'})),
+mask = await Comp.jimp.read('./assets/avatarmask.png'),
+bgmask = await Comp.jimp.read('./assets/bgmask.png'),
+bg = await Comp.jimp.read(row.bg),
+bar = await Comp.jimp.read(new Canvas(700, 20).setColor('#' + (row.accent?row.accent:(bg.getPixelColor(96, 100).toString(16).slice(0, -2)))).addRect(0, 0, Math.ceil(row.xp / (Comp.xpFormule(row.lvl) / 100) * 7), 20).toBuffer()),
+fnt = await Comp.jimp.loadFont('./fonts/uni-sans-heavy-64-white.fnt')
 await avatar.resize(200, 200).mask(mask, 0, 0)
-await Comp.jimp.read('./assets/bgmask.png').then(async bgmask => {
-await Comp.jimp.read(row.bg).then(async bg => {
 await bg.resize(934, 282).blur(5).mask(bgmask)
-await Comp.jimp.read(new Canvas(700, 20).setColor('#' + (row.accent?row.accent:(bg.getPixelColor(96, 100).toString(16).slice(0, -2)))).addRect(0, 0, Math.ceil(length), 20).toBuffer()).then(async bar => {
 await Comp.jimp.read('./assets/xpmask.png').then(async xpmask => await bar.mask(xpmask.resize(700, 20), 0, 0))
 await bg.brightness(-0.5)
 await bar.resize(634, 40); if(Object.values(Comp.owners).includes(user.id)) await Comp.jimp.read('./assets/staff.png').then(async sicon => await bg.composite(sicon, 55, 5))
 await Comp.jimp.read('./assets/'+user.presence.status+'.png').then(async status => await avatar.composite(status, 141, 151))
-await Comp.jimp.loadFont('./fonts/uni-sans-heavy-64-white.fnt').then(async fnt => {
 await bg
 .composite(avatar, 50, 50)
 .composite(bar, 255, 210)
@@ -34,11 +34,12 @@ await bg
 .print(fnt, 655, 0, 'lvl: ' + row.lvl)
 .print(fnt, 350, 50, row.xp + '/' + Comp.xpFormule(row.lvl) + ' xp')
 .print(fnt, 245, 0, 'money:$' + row.money)
-.getBuffer(Comp.jimp.MIME_PNG, async(err, buff) => {
-await message.channel.stopTyping();
-message.channel.send('Made for ' + Math.ceil((Date.now() - timer) / 1000) + ' seconds ', {files: [await new Comp.Discord.MessageAttachment(buff, 'rank.png')]})})})})})})})})}
+.getBuffer(Comp.jimp.MIME_PNG, async (err, buff) => {
+await message.channel.stopTyping()
+message.channel.send('Made for ' + Math.ceil((Date.now() - (message.editedTimestamp?mesaage.editedTimestamp:message.createdTimestamp)) / 1000) + ' seconds ', {files: [await new Comp.Discord.MessageAttachment(buff, 'rank.png')]})
+})}
 const row = Comp.DB.xp.get(user.id)
 if(!row) return message.reply(ph[0])
-if(!['prev', 'preview'].includes(message.args[0])) rcard(row, row.xp / (Comp.xpFormule(row.lvl) / 100) * 7, Date.now())
-else rcard(new Comp.classes.XP({id: message.author.id, lvl: 12, money: 228, xp: 768}), ((768 / ((5 * (12 ^ 2) + 50 * 12 + 100) / 100)) * 7), Date.now())
+if(!['prev', 'preview'].includes(message.args[0])) rcard(row)
+else rcard(new Comp.classes.XP({id: user.id, lvl: 12, money: 228, xp: 768}))
 }
