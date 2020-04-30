69dee72fc01ab98e16d0ddeaaf9dc9043acdbaba
Er2 <er2off@outlook.com>
2020-04-30 21:10:23 +0300

    ok

--- modules/2 (DB).js
+++ modules/2 (DB).js
@@ -29,7 +29,8 @@ if(err) throw err
 rows.forEach(i => {
 let key = Comp.DBid(i, t), val = Comp.DBtables[t]
 i.id = key
-Comp.DB[t].set(key, new Comp.structures.get(val)('', i))
+console.log(Comp.structures, Comp.structures.get(val))
+Comp.DB[t].set(key, new (Comp.structures.get(val))('', i))
 })
 Comp.DBB.set(Comp.DBtables[t]+'Manager', new (Comp.managers.get(Comp.DBtables[t]))(rows, Comp.DBtables[t]))
 console.log('DB loaded',t)
