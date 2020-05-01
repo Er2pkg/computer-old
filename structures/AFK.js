49c3ff7052d76157c885bcf56dc6b6e53e7f7b3d
Er2ch <er2off@outlook.com>
2020-05-01 17:04:58 +0300

    ok

--- structures/AFK.js
+++ structures/AFK.js
@@ -1,8 +1,8 @@
 module.exports = class AFK {
 constructor(a, obj) {
-this.id = obj.id?obj.id:null
 this.yes = obj.yes || 0
 this.reason = obj.reason
+this.id = obj.id?obj.id:null
 this._deleted = false
 }
 }
