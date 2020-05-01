eb84aa86a18aba141b6478bc4b03eb98de3e13d9
Er2ch <er2off@outlook.com>
2020-05-01 21:38:40 +0300

    ok

--- modules/2 (DB).js
+++ modules/2 (DB).js
@@ -2,6 +2,11 @@ module.exports.run = () => {
 console.log('DB init')
 const Collection = Comp.Discord.Collection
 Comp.Collection = Collection
+Comp.DBtables = [
+'AFK', 'Ignore', 'Lang',
+'Mute', 'Note', 'Pred',
+'XP',
+]
 Comp.DBid = (i, t) => {
 let key = i.id?i.id:0
 if(['notes'].includes(t)) key = i.user+'_'+i.id
@@ -10,13 +15,13 @@ return key
 }
 //Comp.DB = new Collection()
 Comp.models = new Collection()
-Comp.schemas = new (require('../schemas/list'))(Comp.db)
-Comp.schemas = Comp.schemas.schemas
-console.log(Comp.schemas)
-Comp.schemas.keyArray().forEach(i => {
+Comp.schemas = new Collection()
+Comp.DBtables.forEach(i => {
+Comp.schemas.set(i, new (require('../schemas/'+i))(Comp.db))
 Comp.models.set(i, Comp.db.model(i, Comp.schemas.get(i)))
 
 console.log('DB loaded',t)
 })
+console.log(Comp.schemas)
 console.log('DB inited')
 }
--- schemas/list.js
+++
@@ -1,6 +0,0 @@
-module.exports = class List {
-constructor(db) {
-this.Discord = require('discord.js')
-this.schemas = new this.Discord.Collection()
-require('fs').readdir(__dirname, (err, data) => data.filter(i => !['list.js'].includes(i)).forEach(i => this.schemas.set(i.slice(0, -3), new (require('./'+i))(db) )))
-}}
