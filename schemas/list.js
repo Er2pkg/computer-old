158ca28dc047c3f5ca0a577536b373ab8eff9e4e
Er2ch <er2off@outlook.com>
2020-05-01 20:58:04 +0300

    hmhm

--- schemas/list.js
+++ schemas/list.js
@@ -1,5 +1,6 @@
 module.exports = db => {
-let schemas = new (require('discord.js')).Collection()
+let Discord = require('discord.js'),
+schemas = new Discord.Collection()
 require('fs').readdir(__dirname, (err, data) => data.filter(i => !['list.js'].includes(i)).forEach(i => schemas.set(i.slice(0, -3), require('./'+i)(db) )))
 return schemas
 }
